36335e09cf4589b154d096e3d45e6ad67d3003b7
Rajagopal Chinnaraj <rajagopal.ceg@gmail.com>
2013-05-10 12:01:20 +0530

    Simple edit to User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ActiveRecord::Base
 	has_many :albums
 	#has_many :medias
 
-  	# attr_accessible :title, :body
-  	def medias
-  		Media.where(:user_id => :id)
-  	end
-  # attr_accessible :title, :body
+  def medias
+  	Media.where(:user_id => :id)
+  end
+
   has_many :posts
   
   attr_accessible :name
